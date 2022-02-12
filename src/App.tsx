9dd952744e66863a70b6e125099a87304dc08479
Dev-akmfop <86141373+MrF1yn@users.noreply.github.com>
2022-02-12 15:21:06 +0530

    added font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import navBar from "./components/NavBar";
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <header className="NavBar-header">
+                FALCON
+            </header>
+        </div>
+    );
+}
+
+export default NavBar;
